264e8955d306d8a1508867ac9e0c4da119f39fd3
Hau Hsueh Chen <hchen057@ucr.edu>
2019-01-20 16:30:12 +0800

    New Butter Fly

--- ExperienceTranquility/Overwatch.js
+++ ExperienceTranquility/Overwatch.js
@@ -1,8 +1,15 @@
 function revealMessage() {
     document.getElementById("hiddenMessage").style.display = 'block';
+    setTimeout(function(){ document.getElementById("hiddenMessage").style.display = "none"; }, 5000);
 }
 
 function playAudio() {
     var sound = document.getElementById("audio");
     sound.play();
 }
+
+function fadelogoout() {
+    //wait(5000);
+    
+}
+
